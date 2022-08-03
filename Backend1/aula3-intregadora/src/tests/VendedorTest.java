02287c4402673924aef93f82d0c32c3fa54ce030
Victor Luz <85527775+vitinop@users.noreply.github.com>
2022-08-03 10:09:31 -0300

    update

--- Backend1/aula3-intregadora/src/tests/VendedorTest.java
+++ Backend1/aula3-intregadora/src/tests/VendedorTest.java
@@ -1,5 +1,25 @@
 package tests;
+import entities.Funcionario;
+import entities.Vendedor;
+import entities.Afiliado;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 
 public class VendedorTest {
+
+    Vendedor vendedor1, vendedor2;
+
+    @BeforeEach
+    void doBefore(){
+        vendedor1 = new Funcionario("Victor",2);
+        vendedor2 = new Funcionario("Junior", 15);
+    }
+
+    @Test
+    void test(){
+        vendedor1.mostrarCategoria();
+        vendedor2.mostrarCategoria();
+    }
 
 }
